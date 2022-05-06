c9d69cd9910d26f08c156acc3de477f090ac190c
Simone Cuozzo <50242695+simone-cuozzo@users.noreply.github.com>
2022-05-06 11:50:40 +0200

    Update data_config.py

--- utils/data_config.py
+++ utils/data_config.py
@@ -8,8 +8,8 @@ import numpy as np
 import mat73
 
 ### dataset configuration from MATLAB matrices. The images matrices are preprocessed with 2 hard thresholds at 0 and 1 ###
-def config(dir, dataset):
-    path = Path(dir + dataset)
+def config(dataset_dir):
+    path = Path(dataset_dir)
     data = loadmat(path)
     data.pop('__header__')
     data.pop('__version__')
